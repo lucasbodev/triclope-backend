b1a61be056869416e357328257dd402746eea14d
lucasbodev <lucasbodet.dev@gmail.com>
2025-09-03 22:10:34 +0200

    first commit

---
+++ src/test/java/com/triclope/TriclopeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triclope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriclopeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
